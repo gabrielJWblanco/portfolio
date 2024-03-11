b70881a51b2143140cd29ef07a66224e13ac730c
gabrielJWblanco <blancog04@gmail.com>
2024-03-11 20:43:01 +0000

    TBC

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,68 +1,8 @@
 import React, { useState } from "react";
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-import { ConfigProvider, Menu } from "antd";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const items = [
-    {
-      label: "Navigation One",
-      key: "mail",
-      icon: <MailOutlined />,
-    },
-    {
-      label: "Navigation Two",
-      key: "app",
-      icon: <AppstoreOutlined />,
-    },
-    {
-      label: "Navigation Three - Submenu",
-      key: "SubMenu",
-      icon: <SettingOutlined />,
-      children: [
-        {
-          type: "group",
-          label: "Item 1",
-          children: [
-            {
-              label: "Option 1",
-              key: "setting:1",
-            },
-            {
-              label: "Option 2",
-              key: "setting:2",
-            },
-          ],
-        },
-        {
-          type: "group",
-          label: "Item 2",
-          children: [
-            {
-              label: "Option 3",
-              key: "setting:3",
-            },
-            {
-              label: "Option 4",
-              key: "setting:4",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: (
-        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-          Navigation Four - Link
-        </a>
-      ),
-      key: "alipay",
-    },
-  ];
   const [current, setCurrent] = useState("mail");
   const onClick = (e) => {
     console.log("click ", e);
@@ -70,31 +10,15 @@ export default function Navbar() {
   };
   return (
     <>
-      {/*  <ConfigProvider
-        theme={{
-          token: {
-            colorBgElevated:"#000000",
-            colorPrimaryBorder:"pink"
-          },
-        }}
-      >
-        <Menu
-          onClick={onClick}
-          selectedKeys={[current]}
-          mode="horizontal"
-          items={items}
-          className="navbar"
-        />
-      </ConfigProvider> */}
       <nav class="nav">
         <input id="menu" type="checkbox" />
         <label for="menu">Menu</label>
         <ul class="menu">
           <li>
-            <a href="#0">
+            <Link>
               <span>Sobre mim</span>
               <i class="fas fa-address-card" aria-hidden="true"></i>
-            </a>
+            </Link>
           </li>
           <li>
             <a href="#0">
