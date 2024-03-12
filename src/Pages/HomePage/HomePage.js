463e87da9d6cf4a8b7d49a94285487980ba530f1
gabrielJWblanco <blancog04@gmail.com>
2024-03-12 18:25:33 +0000

    Home page created

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import { Flex, Image } from "antd";
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import imageLogo from "./Capture.png";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <Flex vertical style={{overflow:"hidden", height:"100%", width:"100%",}}>
+      <Flex flex={1} justify="center" align="center">
+        <TypeAnimation
+          sequence={[
+            2000,
+            "Bem-vindo ao meu portfolio",
+            1000,
+            "Welcome to my portfolio",
+            1000,
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "3rem", display: "inline-block" }}
+          repeat={0}
+        />
+      </Flex>
+
+      <Flex align="center" justify="center" flex={1}>
+        <Image src={imageLogo} style={{height:640}} preview={false}/>
+      </Flex>
+    </Flex>
+  );
 }
